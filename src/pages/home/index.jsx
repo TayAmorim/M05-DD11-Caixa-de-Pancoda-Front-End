1cf8f62995e10b4da18aa8b4f29df410fb76d959
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-09-15 14:03:18 -0300

    Styles: Adicionando top 0 para o modal ficar centralizado

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,21 @@
-import './index.css'
+import { useState } from "react";
+import "./index.css";
+import ModalEditUser from "../../components/modalEdituser";
+import { Button } from "@mui/base";
 
 export default function Home() {
+  const [openModalEditUser, setOpenModaEditUser] = useState(false);
 
-    return (
-        <>
-            <h1>Home</h1>
-
-        </>
-    )
+  return (
+    <>
+      <h1>Home</h1>
+      <Button onClick={() => setOpenModaEditUser(true)}>Abrir</Button>
+      {openModalEditUser && (
+        <ModalEditUser
+          setOpenModaEditUser={setOpenModaEditUser}
+          openModalEditUser={openModalEditUser}
+        />
+      )}
+    </>
+  );
 }
