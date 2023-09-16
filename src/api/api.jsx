ef70e694ed69a00388f4e5c4df32a3178b9af7af
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-09-16 11:42:12 -0300

    feature: Melhorando a perfomace nas requisiçoes que precisem do Beare Token

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,8 +1,17 @@
-import axios from 'axios'
+import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
-    timeout: 10000,
-    headers: {'Content-Type': 'application/json'}
-  });
+  baseURL: "http://localhost:3000",
+  timeout: 10000,
+  headers: { "Content-Type": "application/json" },
+});
 
-  export default api
+api.interceptors.request.use((request) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    request.headers.Authorization = `Bearer ${token}`;
+  }
+  return request;
+});
+
+export default api;
